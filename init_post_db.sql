93d727904a539d55ffcd451f9f73bad234f635bd
Yisyuan Chen <a1888900@adelaide.edu.au>
2024-08-29 22:05:00 +0000

    new file:   init_post_db.sql

---
+++ init_post_db.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS postal;
+CREATE DATABASE postal;
+
+USE postal;
+
+CREATE TABLE pcode(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    postcode CHAR(4) NOT NULL,
+    locality VARCHAR(40) NOT NULL,
+    state CHAR(3) NOT NULL
+);
